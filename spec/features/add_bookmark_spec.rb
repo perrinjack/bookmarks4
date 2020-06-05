fb23bd5772b5182ed355158afb4f26a81106f965
J Perrin <perrinjack96@gmail.com>
2020-06-05 15:58:57 +0100

    update feature test done

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -25,3 +25,15 @@ feature 'deletes a bookmark' do
     expect(page).not_to have_link('Google', href: 'http//www.google.com') 
   end
 end
+
+  feature 'updates a bookmark' do
+    scenario 'updates an existing bookmark' do
+    visit('/bookmarks')
+    Bookmark.create('www.premierleague.com', 'Prem')
+    first('.bookmark').click_button 'Update'
+    fill_in('title', with: 'Playboy')
+    fill_in('url', with: 'Playboy.com')
+    click_button('Update')
+    expect(page).to have_link('Playboy', href: 'http://www.Playboy.com')
+    end 
+end
